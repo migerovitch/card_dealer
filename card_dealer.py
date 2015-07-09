bb5d9fba3dce4242ea14c2d39a64e2bf3455bdbe
Anthony <anoky96@gmail.com>
2015-07-08 21:35:35 -0400

    now sets up a deck of cards

--- card_dealer.py
+++ card_dealer.py
@@ -1,7 +1,8 @@
 from card import Card
 
-def main():
-	
-
-if __name__ == '__main__':
-	main()
+class CardDealer:
+	def __init__():
+		cards = []
+		for i in range(3):
+			for j in range(12):
+				cards.insert(Card(i, j))
