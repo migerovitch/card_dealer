8e0b4f0581982af464a23e53a5482b8114194208
SirPython <SirPython@users.noreply.github.com>
2015-07-08 20:58:17 -0400

    Setup a Value and Suit enum and a Card class.

---
+++ card.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit;
+        self.value = value;
+
+class Suit(Enum):
+    DIAMONDS = 0
+    CLUBS = 1
+    HEARTS = 2
+    SPADES = 3
+
+class Value(Enum):
+    ACE = 0
+    TWO = 1
+    THREE = 2
+    FOUR = 3
+    FIVE = 4
+    SIX = 5
+    SEVEN = 6
+    EIGHT = 7
+    NINE = 8
+    TEN = 9
+    JACK = 10
+    QUEEN = 11
+    KING = 12
