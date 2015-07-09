af928fa66c097a3889b55a76a247b2df0add5314
Anthony <anoky96@gmail.com>
2015-07-08 21:40:37 -0400

    fixed typo in Card docstring, added CardDealer docstring

--- card.py
+++ card.py
@@ -6,8 +6,9 @@ class Card:
 	A class for holding the information of a single Card
 	@param(number) - the suit of the card
 	@param(number) - the value of the card
+	@param(boolean) - whether or not the card has been used
 	"""
     def __init__(self, suit, value, used=False):
-        self.suit = suit;
+        self.suit = suit
         self.value = value
-        self.used = used;
+        self.used = used
--- card_dealer.py
+++ card_dealer.py
@@ -1,6 +1,9 @@
 from card import Card
 
 class CardDealer:
+	"""
+	A class for dealing and handling groups of cards
+	"""
 	def __init__():
 		cards = []
 		for i in range(3):
